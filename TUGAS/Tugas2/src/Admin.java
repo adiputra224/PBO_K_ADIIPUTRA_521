fcf8437f0f8d4fe819b041ee8bbf8b9af10cd7b1
adiputra224 <adiputra224>
2025-04-16 18:20:15 +0700

    update

--- TUGAS/Tugas2/src/Admin.java
+++
@@ -1,9 +0,0 @@
-public class Admin {
-    private String username = "admin";
-    private String password = "1234";
-
-    // Metode untuk login admin
-    public boolean login(String inputUsername, String inputPassword) {
-        return inputUsername.equals(username) && inputPassword.equals(password);
-    }
-}
